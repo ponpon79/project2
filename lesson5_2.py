430f3840b8963e72d5eb7d2da4daab797c0c38a5
ponpon <peng850709@gmail.com>
2022-11-12 11:14:50 +0800

    猜數字完成

---
+++ lesson5_2.py
@@ -0,0 +1,25 @@
+import random
+
+min = 1
+max = 100
+count = 0
+random_value = random.randint(min,max)
+print("=======猜數字遊戲=======")
+while True:
+    count += 1
+    keyin = int(input(f"猜數字的範圍{min}~{max}:"))
+    if keyin >= min and keyin <= max:
+        if(keyin == random_value):
+            print(f"猜對了,答案是:{random_value}")
+            print(f"您總共猜{count}次")
+            break
+        elif keyin > random_value:
+            print("再小一點")
+            max = keyin - 1
+        elif keyin < random_value:
+            print("再大一點")
+            min = keyin + 1
+        print(f"您已經猜了{count}次")
+    else:
+        print("超出範圍")
+print("遊戲結束")
